e16eb9a01636e33b10a1fbe67e35cc864b229ceb
a.mirarchi <antonio.mirarchi@cloudfinance.it>
2020-10-26 15:02:12 +0100

    Minfix

--- src/Engines/Base.php
+++ src/Engines/Base.php
@@ -238,7 +238,7 @@ abstract class Base {
                 if (!empty($action)){
                     $funName="listen".ucfirst(Str::camel($action));
                     if (method_exists($this, $funName)){	
-                        return route("pym-magic-box", array_merge($params,["merchant" => $this->performer->merchant_id, "method" => Str::kebab($this->performer->method->name), "action" => $action], $absolute);
+                        return route("pym-magic-box", array_merge($params,["merchant" => $this->performer->merchant_id, "method" => Str::kebab($this->performer->method->name), "action" => $action]), $absolute);
                     }
                 }
 		return null;
