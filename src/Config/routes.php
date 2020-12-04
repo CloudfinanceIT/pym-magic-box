d509eb2e4ba7c76d5187783aa2c7a5fe06000870
a.mirarchi <antonio.mirarchi@cloudfinance.it>
2020-12-04 13:29:04 +0100

    minifix routes

--- src/Config/routes.php
+++ src/Config/routes.php
@@ -1,3 +1,3 @@
 <?php
 \Route::match(['get', 'post'],"/pym-magic-box/{merchant}/{method}/{action}","\Mantonio84\pymMagicBox\Controllers\pymListenController@merchantAction")->name("pym-magic-box.merchant-action");
-\Route::post(,"/pym-wb/{merchant}/{method}","\Mantonio84\pymMagicBox\Controllers\pymListenController@webhook")->name("pym-magic-box.webhook");
+\Route::post("/pym-wb/{merchant}/{method}","\Mantonio84\pymMagicBox\Controllers\pymListenController@webhook")->name("pym-magic-box.webhook");
