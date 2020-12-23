3b87b5f72a0dfc4fbd1e5527921e4d4620bce203
a.mirarchi <antonio.mirarchi@cloudfinance.it>
2020-12-23 11:54:05 +0100

    Migliorato log eccezioni

--- src/Exceptions/pymMagicBoxException.php
+++ src/Exceptions/pymMagicBoxException.php
@@ -11,25 +11,47 @@ class pymMagicBoxException extends \Exception {
     protected $plog_merchant_id=null;
     protected $plog_level=null;
     protected $plog_params=[];
+	protected $logged="";
     
     public function __construct(string $message = "", int $code = 0, \Throwable $previous = NULL) {        
         parent::__construct($message, $code, $previous);        
         if (!empty($message)){
-            $this->loggable(null, null, ["message" => class_basename($this)." ::: ".$this->getMessage(), "details" => $this->getTraceAsString()]);
+			$this->plog_params=[
+				"message" => class_basename($this)." ::: ".$this->getMessage(),
+				"details" => $this->getTraceAsString()
+			];            
         }
     }
     
-    public function loggable($level=null, $merchant_id=null, $params=null){
+    public function loggable($level=null, $merchant_id=null, $params=null){		
         $this->plog_level=$this->plog_level ?? $level;
         $this->plog_merchant_id=$this->plog_merchant_id ?? $merchant_id;
-        $this->plog_params=array_merge(is_array($this->plog_params) ? $this->plog_params : [], is_array($params) ? $params : []);
+        $this->plog_params=array_merge(is_array($this->plog_params) ? $this->plog_params : [], is_array($params) ? $params : []);		
+		$this->writeToPymLog();		
         return $this;
     }
     
     
     public function report() {
-        if (!is_null($this->plog_merchant_id) && !is_null($this->plog_level)){
-            pmbLogger::make()->write($this->plog_level, $this->plog_merchant_id, $this->plog_params);
-        }        
+        $this->writeToPymLog();
     }
+	
+	protected function writeToPymLog(bool $forced=false){
+		$p=$this->logDataFingerpint();
+		if (!empty($p) && ($this->logged!=$p || $forced)){
+            pmbLogger::make()->write($this->plog_level, $this->plog_merchant_id, $this->plog_params);
+			$this->logged=$p;
+        }        
+	}
+	
+	protected function isLoggable(){
+		return (!is_null($this->plog_merchant_id) && !is_null($this->plog_level) && isset($this->plog_params['message']));
+	}
+	
+	protected function logDataFingerpint(){
+		if (!$this->isLoggable()){
+			return "";
+		}
+		return md5(json_encode([$this->plog_merchant_id,$this->plog_level,$this->plog_params]));
+	}
 }
