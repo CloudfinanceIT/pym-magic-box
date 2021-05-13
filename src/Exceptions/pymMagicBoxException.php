1c6ca989b877bf8304ca040928a7425b40d35ad9
a.mirarchi <antonio.mirarchi@cloudfinance.it>
2021-05-13 15:46:33 +0200

    getLastLoggedEvent

--- src/Exceptions/pymMagicBoxException.php
+++ src/Exceptions/pymMagicBoxException.php
@@ -12,7 +12,8 @@ class pymMagicBoxException extends \Exception {
     protected $plog_level=null;
     protected $plog_params=[];
 	protected $logged="";
-    
+    protected $last_logged;
+	
     public function __construct(string $message = "", int $code = 0, \Throwable $previous = NULL) {        
         parent::__construct($message, $code, $previous);        
         if (!empty($message)){
@@ -36,10 +37,15 @@ class pymMagicBoxException extends \Exception {
         $this->writeToPymLog();
     }
 	
+	public function getLastLoggedEvent(){
+		$this->writeToPymLog();
+		return $this->last_logged;
+	}
+	
 	protected function writeToPymLog(bool $forced=false){
 		$p=$this->logDataFingerpint();
 		if (!empty($p) && ($this->logged!=$p || $forced)){
-            pmbLogger::make()->write($this->plog_level, $this->plog_merchant_id, $this->plog_params);
+            $this->last_logged=pmbLogger::make()->write($this->plog_level, $this->plog_merchant_id, $this->plog_params);
 			$this->logged=$p;
         }        
 	}
