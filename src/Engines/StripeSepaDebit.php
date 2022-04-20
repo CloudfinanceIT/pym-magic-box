c70706acc0142722322194a51e76519e7b62b4a2
Agostino Pagnozzi <a.pagnozzi@cloudfinance.it>
2022-04-20 18:03:30 +0200

    Creazione nuovi metodi di pagamento per Stripe

---
+++ src/Engines/StripeSepaDebit.php
@@ -0,0 +1,81 @@
+<?php
+namespace Mantonio84\pymMagicBox\Engines;
+
+use \Mantonio84\pymMagicBox\Classes\processPaymentResponse;
+use \Mantonio84\pymMagicBox\Models\pmbPayment;
+use \Mantonio84\pymMagicBox\Models\pmbAlias;
+
+/**
+ * Classe per il pagemnto con mandato Sepa Debit tramite 
+ * Stripe.
+ * 
+ * @author Agostino Pagnozzi
+ */
+class StripeSepaDebit extends Base 
+{
+    public static function autoDiscovery()
+    {
+        return [
+            "name" => "stripe_sepa_debit",          
+        ];
+    }
+    
+    public function isConfirmable(pmbPayment $payment): bool
+    {
+        return ($payment->billed && !$payment->confirmed && $payment->refunded_amount == 0);
+    }
+
+    protected function onProcessPaymentConfirm(pmbPayment $payment, array $data = []): bool
+    {
+        // TODO...
+    }
+
+    protected function onProcessAliasDelete(pmbAlias $alias): bool
+    {
+        // TODO...
+    }
+
+    protected function onProcessAliasConfirm(pmbAlias $alias, array $data = []): bool
+    {
+        return false;
+    }
+
+    protected function onProcessAliasCreate(array $data, string $name, string $customer_id = "", $expires_at = null)
+    {
+        // TODO...
+    }
+
+    protected function onProcessRefund(pmbPayment $payment, float $amount, array $data = []): bool
+    {
+        // TODO...
+    }
+
+    protected function validateConfig(array $config)
+    {
+        // TODO...
+    }
+
+    public function isAliasConfirmable(pmbAlias $alias): bool
+    {
+        return false;
+    }
+
+    public function supportsAliases(): bool
+    {
+        return true;
+    }
+
+    protected function onProcessPayment(pmbPayment $payment, $alias_data, array $data = [], string $customer_id): processPaymentResponse
+    {
+        // TODO...
+    }
+
+    public function isRefundable(pmbPayment $payment): float
+    {
+        if (!$payment->billed && !$payment->confirmed) {
+            return 0;
+        }
+        
+        return $payment->refundable_amount;
+    }
+}
--- src/Views/redirectform.blade.php
+++ src/Views/redirectform.blade.php
@@ -1,18 +1,18 @@
 <html>
     <body>
         @php
-        $id=uniqid("pmb-redirect-form-");
+        	$id=uniqid("pmb-redirect-form-");
         @endphp
-        <form action="{{$action}}" method="{{$method}}" id="{{$id}}">
+        <form action="{{ $action }}" method="{{ $method }}" id="{{ $id }}">
             @foreach($fields as $name => $value)
-            <input type="hidden" name="{{$name}}" value="{{$value}}">
+            <input type="hidden" name="{{ $name }}" value="{{ $value }}">
             @endforeach
             <input type="submit" value="Se non vieni ridirezionato entro 5 secondi, fai click qui" style="border: 0;background-color:transparent;cursor:hand;color:blue;text-decoration:underline">            
         </form>
-        <script type="text/javascript">
-            setTimeout(function (){
-		document.getElementById("{{$id}}").submit();
-            },150);
+        <script>
+            setTimeout(function () {
+				document.getElementById("{{ $id }}").submit();
+            }, 150);
         </script>
     </body>
 </html>
